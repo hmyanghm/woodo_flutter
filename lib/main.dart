d5b6e4e8ab15022645517eeac48c0b0edf9e12e3
YHM <hmyanghm@gmail.com>
2022-08-22 09:40:27 +0900

    첫 로딩 화면 수정 테스트

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,71 @@ void main() {
   runApp(const MyApp());
 }
 
-class Init {
-  Init._();
-  static final instance = Init._();
+// class Init {
+//   Init._();
+//   static final instance = Init._();
 
-  Future<Widget> initialize(BuildContext context) async {
-    return SplashScreen();
-  }
-}
+//   Future<Widget> initialize(BuildContext context) async {
+//     return SplashScreen();
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        FocusManager.instance.primaryFocus?.unfocus();
-      },
-      child: MaterialApp(
-        color: Colors.amber,
-        title: 'WOODO',
-        theme: ThemeData(
-          primaryColor: Colors.white,
-          primarySwatch: Colors.amber,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          fontFamily: 'main',
-        ),
-        home: SplashScreen(),
-      ),
-    );
+    return FutureBuilder(
+        future: Init.instance.initialize(context),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(
+              home: SplashScreen(),
+            );
+          } else if (snapshot.hasError) {
+            return MaterialApp(
+              home: MyApp(),
+            );
+          } else {
+            return MaterialApp(
+              title: 'WOODO',
+              theme: ThemeData(primarySwatch: Colors.amber),
+              home: snapshot.data,
+              builder: (context, child) => MediaQuery(
+                data: MediaQuery.of(context).copyWith(
+                    textScaleFactor: MediaQuery.of(context)
+                        .textScaleFactor
+                        .clamp(0.95, 1.05)),
+                child: child!,
+              ),
+            );
+          }
+        });
+  }
+}
+//   return GestureDetector(
+//     onTap: () {
+//       FocusManager.instance.primaryFocus?.unfocus();
+//     },
+//     child: MaterialApp(
+//       color: Colors.amber,
+//       title: 'WOODO',
+//       theme: ThemeData(
+//         primaryColor: Colors.white,
+//         primarySwatch: Colors.amber,
+//         visualDensity: VisualDensity.adaptivePlatformDensity,
+//         fontFamily: 'main',
+//       ),
+//       home: SplashScreen(),
+//     ),
+//   );
+// }
+
+class Init {
+  Init._();
+  static final instance = Init._();
+
+  Future<Widget> initialize(BuildContext context) async {
+    return SplashScreen();
   }
 }
