748b4c0df00670eb797ee97ae00764b7b311b6ef
YHM <hmyanghm@gmail.com>
2022-08-31 16:54:40 +0900

    intro page 생성

--- lib/page/app.dart
+++ lib/page/app.dart
@@ -2,11 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:woodo/page/chat.dart';
 import 'package:woodo/page/list.dart';
 import 'package:woodo/page/mypage.dart';
-import 'package:woodo/widget/appBar.dart';
-import 'package:woodo/widget/pageView.dart';
+import 'package:woodo/widget/pageSlide.dart';
 
 import 'favorite.dart';
-import 'home.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -16,73 +14,71 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int _currentPageIndex = 0;
+//   int _currentPageIndex = 0;
 
-  Widget _bodyWidget() {
-    switch (_currentPageIndex) {
-      case 0:
-        // return Home();
-        return PageViewWidget();
-      case 1:
-        return MainList();
-      case 2:
-        return Chat();
-      case 3:
-        return MyFavorite();
-      case 4:
-        return MyPage();
-    }
-    return Container();
-  }
+//   Widget _bodyWidget() {
+//     switch (_currentPageIndex) {
+//       case 0:
+//         // return Home();
+//         // return PageViewWidget();
+//         return PageSlideWidget();
+//       case 1:
+//         return MainList();
+//       case 2:
+//         return Chat();
+//       case 3:
+//         return MyFavorite();
+//       case 4:
+//         return MyPage();
+//     }
+//     return Container();
+//   }
 
-  BottomNavigationBarItem _bottomNavigationBarItem(
-      String iconName, String label) {
-    return BottomNavigationBarItem(
-      icon: Padding(
-        padding: const EdgeInsets.only(bottom: 5),
-        child: Image.asset(
-          'assets/icons/$iconName.png',
-          width: 22,
-        ),
-      ),
-      activeIcon: Padding(
-        padding: const EdgeInsets.only(bottom: 5),
-        child: Image.asset(
-          'assets/icons/${iconName}_on.png',
-          width: 22,
-        ),
-      ),
-      label: label,
-    );
-  }
+//   BottomNavigationBarItem _bottomNavigationBarItem(
+//       String iconName, String label) {
+//     return BottomNavigationBarItem(
+//       icon: Padding(
+//         padding: const EdgeInsets.only(bottom: 5),
+//         child: Image.asset(
+//           'assets/icons/$iconName.png',
+//           width: 22,
+//         ),
+//       ),
+//       activeIcon: Padding(
+//         padding: const EdgeInsets.only(bottom: 5),
+//         child: Image.asset(
+//           'assets/icons/${iconName}_on.png',
+//           width: 22,
+//         ),
+//       ),
+//       label: label,
+//     );
+//   }
 
-  Widget _bottomNavigationBarWidget() {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      onTap: (int index) {
-        setState(() {
-          _currentPageIndex = index;
-        });
-        print(index);
-      },
-      currentIndex: _currentPageIndex,
-      items: [
-        _bottomNavigationBarItem('home', '홈'),
-        _bottomNavigationBarItem('pin', '우리동네'),
-        _bottomNavigationBarItem('chat', '채팅'),
-        _bottomNavigationBarItem('good', '관심도서'),
-        _bottomNavigationBarItem('mypage', '마이페이지'),
-      ],
-    );
-  }
+//   Widget _bottomNavigationBarWidget() {
+//     return BottomNavigationBar(
+//       type: BottomNavigationBarType.fixed,
+//       onTap: (int index) {
+//         setState(() {
+//           _currentPageIndex = index;
+//         });
+//         print(index);
+//       },
+//       currentIndex: _currentPageIndex,
+//       items: [
+//         _bottomNavigationBarItem('home', '홈'),
+//         _bottomNavigationBarItem('pin', '우리동네'),
+//         _bottomNavigationBarItem('chat', '채팅'),
+//         _bottomNavigationBarItem('good', '관심도서'),
+//         _bottomNavigationBarItem('mypage', '마이페이지'),
+//       ],
+//     );
+//   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: BaseAppBar(title: "우리동네 도서관", appBar: AppBar()),
-      body: _bodyWidget(),
-      bottomNavigationBar: _bottomNavigationBarWidget(),
+      body: PageSlideWidget(),
     );
   }
 }
