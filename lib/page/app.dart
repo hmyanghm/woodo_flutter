119fa95558412ab69bbd24fc42db019cc4d9cbb4
YHM <hmyanghm@gmail.com>
2022-09-08 12:59:30 +0900

    firebase 연결

--- lib/page/app.dart
+++ lib/page/app.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:woodo/page/chat.dart';
-import 'package:woodo/page/list.dart';
-import 'package:woodo/page/mypage.dart';
 import 'package:woodo/widget/pageSlide.dart';
-
-import 'favorite.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -14,67 +9,6 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-//   int _currentPageIndex = 0;
-
-//   Widget _bodyWidget() {
-//     switch (_currentPageIndex) {
-//       case 0:
-//         // return Home();
-//         // return PageViewWidget();
-//         return PageSlideWidget();
-//       case 1:
-//         return MainList();
-//       case 2:
-//         return Chat();
-//       case 3:
-//         return MyFavorite();
-//       case 4:
-//         return MyPage();
-//     }
-//     return Container();
-//   }
-
-//   BottomNavigationBarItem _bottomNavigationBarItem(
-//       String iconName, String label) {
-//     return BottomNavigationBarItem(
-//       icon: Padding(
-//         padding: const EdgeInsets.only(bottom: 5),
-//         child: Image.asset(
-//           'assets/icons/$iconName.png',
-//           width: 22,
-//         ),
-//       ),
-//       activeIcon: Padding(
-//         padding: const EdgeInsets.only(bottom: 5),
-//         child: Image.asset(
-//           'assets/icons/${iconName}_on.png',
-//           width: 22,
-//         ),
-//       ),
-//       label: label,
-//     );
-//   }
-
-//   Widget _bottomNavigationBarWidget() {
-//     return BottomNavigationBar(
-//       type: BottomNavigationBarType.fixed,
-//       onTap: (int index) {
-//         setState(() {
-//           _currentPageIndex = index;
-//         });
-//         print(index);
-//       },
-//       currentIndex: _currentPageIndex,
-//       items: [
-//         _bottomNavigationBarItem('home', '홈'),
-//         _bottomNavigationBarItem('pin', '우리동네'),
-//         _bottomNavigationBarItem('chat', '채팅'),
-//         _bottomNavigationBarItem('good', '관심도서'),
-//         _bottomNavigationBarItem('mypage', '마이페이지'),
-//       ],
-//     );
-//   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
